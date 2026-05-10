d62591d592ed532325a73b70255744468afff1d8
okmic <okmic.g@gmail.com>
2026-05-10 19:57:54 +0300

    loading screen and logo

---
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -0,0 +1,23 @@
+import "./styles.css"
+
+export default function LoadingScreen({ onClick, isExiting }: { onClick: () => void, isExiting: boolean }) {
+    const exStyle = isExiting
+      ? 'opacity-0 scale-105 blur-md pointer-events-none'
+      : 'opacity-100 scale-100 blur-0'
+
+    return <div className={`fixed w-screen h-screen flex flex-col justify-center items-center bg-[#000] ${exStyle}`}>
+    <div className="container" onClick={onClick}>
+        <div className="logo-masterpiece">
+            <div className="logo-pulse"></div>
+            <div className="logo-glow"></div>
+            <div className="logo-core">
+                <div className="logo-symbol">M</div>
+            </div>
+        </div>
+
+        <div className="typography">
+            <div className="brand-name">MicoNext</div>
+        </div>
+    </div>
+    </div>
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/offer/',
+  base: '/miconext/',
   build: {
     outDir: './build',
     emptyOutDir: true
